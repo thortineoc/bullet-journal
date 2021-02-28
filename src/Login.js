7b30734dfe9115054396068df7884619e648e5ec
Angelika Kubicka <angkubicka99@gmail.com>
2021-02-28 19:55:56 +0100

    Login page

--- src/Login.js
+++ src/Login.js
@@ -1,16 +1,33 @@
-import React from 'react'
-import './css/Login.css'
+import React from 'react';
+import './css/Login.css';
+import {Button, Input, InputLabel} from '@material-ui/core';
+import {Link} from 'react-router-dom';
 
 export default function Login() {
     return (
         <div class="login">
-            <h1>Login</h1>
-            <form>
-                <label for="login__email">Email</label>
-                <input type="email" id="login__email" name="login__email" />
-                <label for="login__password">Email</label>
-                <input type="password" id="login__password" name="login__password" />
-            </form>
+
+            <div className="login__links">
+                <Link class="login__link" to='/'>Home page</Link>
+                <Link class="login__link" to='/register'>Register</Link>
+            </div>
+
+            <div class="login__container">
+                <h1 class="login__title">Login</h1>
+                <form class="login__form">
+                    <div class="login__row">
+                        <InputLabel for="login__email">Email</InputLabel>
+                        <Input type="email" id="login__email" name="login__email" />
+                    </div>
+                    <div class="login__row">
+                        <InputLabel for="login__password">Password</InputLabel>
+                        <Input type="password" id="login__password" name="login__password" />
+                    </div>
+                    <div className="login__row">
+                        <Button>Login</Button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
