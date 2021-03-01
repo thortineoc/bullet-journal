cbc3a6e0ccd416b41fc3363f5d5fd67ec0827afe
Angelika Kubicka <angkubicka99@gmail.com>
2021-03-01 13:10:01 +0100

    Login logic

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './css/Login.css';
 import {Button, Input, InputLabel} from '@material-ui/core';
 import {Link} from 'react-router-dom';
 
 export default function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = (e) => {
+        e.preventDefault();
+        
+    }
+
     return (
         <div class="login">
 
@@ -17,14 +26,14 @@ export default function Login() {
                 <form class="login__form">
                     <div class="login__row">
                         <InputLabel for="login__email">Email</InputLabel>
-                        <Input type="email" id="login__email" name="login__email" />
+                        <Input type="email" id="login__email" name="login__email" value={email} onChange={e => setEmail(e.target.value)} />
                     </div>
                     <div class="login__row">
                         <InputLabel for="login__password">Password</InputLabel>
-                        <Input type="password" id="login__password" name="login__password" />
+                        <Input type="password" id="login__password" name="login__password" value={password} onChange={e => setPassword(e.target.value)} />
                     </div>
                     <div className="login__row">
-                        <Button>Login</Button>
+                        <Button onClick={signIn} type="submit">Login</Button>
                     </div>
                 </form>
             </div>
